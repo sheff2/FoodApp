3233a79946f5c7ca79c62af0a4b4980269d6eaa5
sheff2 <shaheff01@gmail.com>
2024-05-25 14:56:12 -0400

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Shaun Heffernan on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
